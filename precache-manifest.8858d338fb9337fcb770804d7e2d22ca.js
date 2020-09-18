354ff7c59b7b389bda33638046f131c0b4c891e9
nphaxayaseng <nphaxayaseng@gmail.com>
2020-09-18 10:37:51 -0700

    Updates

---
+++ precache-manifest.8858d338fb9337fcb770804d7e2d22ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984d438120147e0c82799953715120c2",
+    "url": "/hacker-news-clone/index.html"
+  },
+  {
+    "revision": "0a95d1ff14f1c18ffcf8",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-clone/static/js/2.01a2b292.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85900c50eed8ea446bd1",
+    "url": "/hacker-news-clone/static/js/main.bbc94f46.chunk.js"
+  },
+  {
+    "revision": "c9a3baf2b0bc3fd6620c",
+    "url": "/hacker-news-clone/static/js/runtime-main.033094b6.js"
+  }
+]);
